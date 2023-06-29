6edd42cd8a72908a332b8a007e88520a2029333a
Sadak <s.sadak02@gmail.com>
2023-06-29 11:01:37 +0600

    Доавление метода с заполнением массива и распечаткой массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -8,6 +8,23 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Введите строку {i + 1}: ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    foreach (var item in array)
+    {
+        System.Console.Write($"{item}; ");
+    }
+}
+
 Console.Clear();
 System.Console.WriteLine("Введите количество строк: ");
 int num = Convert.ToInt32(Console.ReadLine());
